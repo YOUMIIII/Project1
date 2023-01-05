07fa96804d7f0f7c9d2d93b8bb99b3466807104c
YOUMIIII <youmi8255@naver.com>
2023-01-05 16:15:14 +0900

    220105 폰트class 수정

--- src/Pjt_1_Home/MyFont.java
+++ src/Pjt_1_Home/MyFont.java
@@ -3,16 +3,20 @@ package Pjt_1_Home;
 import java.awt.Font;
 
 public class MyFont {
-	Font f1, f2, f3, f2p, fLogLabel, fPlusLabel, fExLabel, fPlusMenuList, fNorthLabel;
+	Font f1, f2, f3, f2p, f16,f16b, f15, fLogLabel, fPlusLabel, fExLabel, fPlusMenuList, fNorthLabel, f18;
 	MyFont(){
-		f1 = new Font("맑은 고딕", Font.PLAIN, 23);
-		f2 = new Font("맑은 고딕", Font.BOLD, 13); // 버튼용
-		f2p = new Font("맑은 고딕", Font.PLAIN, 13); // 버튼용
-		f3 = new Font("맑은 고딕", Font.BOLD, 17); // 메인페이지 이름용
-		fLogLabel = new Font("맑은 고딕", Font.BOLD, 15);
-		fPlusLabel = new Font("맑은 고딕", Font.BOLD, 15); // 메뉴추가 레이블용
-		fPlusMenuList = new Font("맑은 고딕", Font.PLAIN, 15); // 메뉴추가 레이블용
-		fExLabel = new Font("맑은 고딕", Font.BOLD, 15);
-		fNorthLabel = new Font("맑은 고딕", Font.PLAIN, 16);
+		f1 = new Font("나눔고딕", Font.PLAIN, 23);
+		f2 = new Font("나눔고딕", Font.BOLD, 13); // 버튼용
+		f2p = new Font("나눔고딕", Font.PLAIN, 13); // 버튼용
+		f3 = new Font("나눔고딕", Font.BOLD, 17); // 메인페이지 이름용
+		fLogLabel = new Font("나눔고딕", Font.BOLD, 15);
+		fPlusLabel = new Font("나눔고딕", Font.BOLD, 15); // 메뉴추가 레이블용
+		fPlusMenuList = new Font("나눔고딕", Font.PLAIN, 15); // 메뉴추가 레이블용
+		fExLabel = new Font("나눔고딕", Font.BOLD, 15);
+		fNorthLabel = new Font("나눔고딕", Font.PLAIN, 16);
+		f18 = new Font("나눔고딕", Font.PLAIN, 18);
+		f16 = new Font("나눔고딕",Font.PLAIN, 16);
+		f16b = new Font("나눔고딕",Font.BOLD, 16);
+		f15 = new Font("나눔고딕",Font.PLAIN, 15);
 	}
 }
