a5eebfad03dd965cfb5e863fa62ea6d74be7918d
YOUMIIII <youmi8255@naver.com>
2023-01-06 11:39:13 +0900

    230106 영양소 색깔 클래스

--- src/Pjt_1_ConnectServer/PanelGroup.java
+++ src/Pjt_1_ConnectServer/PanelGroup.java
@@ -14,12 +14,15 @@ public class PanelGroup {
 	public PanelGroup() {
 		back = new Color(129, 129, 129);
 		cNutri = new Color[7];
-		cNutri[0] = new Color(255,243,138);  //탄수화물
-		cNutri[1] = new Color(255,155,5);  //단백질
-		cNutri[2] = new Color(42,255,102);  //채소
-		cNutri[3] = new Color(46,155,255);  //칼슘
-		cNutri[4] = new Color(186,0,252);  //지방
-		cNutri[5] = new Color(255,137,187);  //과일
+		cNutri[1] = new Color(255,135,135);  //탄수화물 빨강
+//		cNutri[0] = new Color(255,243,138);  //탄수화물 노랑일
+		cNutri[0] = new Color(254, 208, 73); //단백질
+//		cNutri[1] = new Color(250,171,120);  //단백질 주황
+		cNutri[2] = new Color(173,231,146);  //채소
+		cNutri[3] = new Color(127,181,255);  //칼슘
+		cNutri[4] = new Color(209,140,224);  //지방
+		cNutri[5] = new Color(255,187,187);  //과일
+//		cNutri[5] = new Color(255,137,187);  //과일
 		cNutri[6] = new Color(2,79,151);  //기타
 	
 		pNutri = new JPanel[7];
